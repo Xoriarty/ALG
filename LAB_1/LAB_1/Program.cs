94cdbc8e381b202fb06ebfbf76fc99c92da40b2e
Irina <xoriartycode@gmail.com>
2025-02-19 01:52:53 +0500

    edit main

--- LAB_1/LAB_1/Program.cs
+++ LAB_1/LAB_1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LAB_1.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,54 @@ namespace LAB_1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите размер массива: ");
+            string line = Console.ReadLine();
+            if (!int.TryParse(line, out int N)) Console.WriteLine("Некорректный ввод числа");
+            else
+            {
+                if (N < 1) Console.WriteLine("Некорректный размер массива (N < 1)");
+                else
+                {
+                    Console.Write("Введите диапазон заполнения массива через пробел: ");
+                    string[] range = Console.ReadLine().Split();
+                    if (range.Length == 2)
+                    {
+                        if (!int.TryParse(range[0], out int start) || !int.TryParse(range[1], out int end)) Console.WriteLine("Некорректный ввод чисел");
+                        else
+                        {
+                            if (start > end)
+                            {
+                                int tmp = start;
+                                start = end;
+                                end = tmp;
+                            }
+                            int[] A = ArrayActions.CreaeteArray(N, start, end);
+                            Console.WriteLine("Сформированный массив: ");
+                            ArrayActions.ViewArray(A);
+                            int[] B = ArrayActions.Sort(A);
+                            Console.WriteLine("\nОтсоритрованный массив: ");
+                            ArrayActions.ViewArray(B);
+                            Console.Write("\nВведите элемент для поиска: ");
+                            if (!int.TryParse(Console.ReadLine(), out int el)) Console.WriteLine("Некорректное число");
+                            else
+                            {
+                                int index = ArrayActions.BinarySearch(B, el);
+                                Console.WriteLine($"Результат с использованием бинарного поиска: {index}");
+                                index = ArrayActions.InterpolationSearch(B, el);
+                                Console.WriteLine($"Результат с использованием интерполяционного поиска: {index}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод");
+                    }
+                }
+            }
 
+
+
+            Console.ReadKey();
         }
     }
 }
