72007cf5cf4f88155358a68820dd5fea546c5e0f
Irina <xoriartycode@gmail.com>
2025-02-19 05:29:59 +0500

    add QuickSort

--- LAB_1/LAB_1/Classes/ArraySortMethods.cs
+++ LAB_1/LAB_1/Classes/ArraySortMethods.cs
@@ -53,6 +53,39 @@ namespace LAB_1.Classes
             }
         }
 
+        // Быстрая сортировка
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                QuickSort(arr, left, pivot - 1);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[right];
+            arr[right] = temp1;
+            return i + 1;
+        }
 
         public static void ViewArray(int[] array)
         {
