ca7b224e652c9f6005424481fc5d75eacc7c2e0d
Irina <xoriartycode@gmail.com>
2025-04-04 01:10:45 +0500

    Проект для лабы 2

---
+++ LAB_2/LAB_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LAB_2
+{
+    internal class Program
+    {
+        static decimal Rec(int n, decimal res = 0)
+        {
+            if (n == 0) return res;
+            decimal a = n * Convert.ToDecimal(Math.Sqrt(Convert.ToDouble(n + 5 + res)));
+            return Rec(n - 1, a);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ответ: {Rec(n)}");
+            Console.ReadKey();
+        }
+    }
+}
